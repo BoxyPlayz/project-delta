9c957b51326942892a8d1eb453af4d2cdafb19f9
Boxy <boxy@myyahoo.com>
2026-08-11 17:43:56 -0500

    udpate url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "boxyplayz.github.io",
+	site: "https://boxyplayz.github.io",
 	base: "project-delta"
 });
